339c6e51fccead00c668dc33ab44ea19908acaa3
自有计算（Self-Sovereign Computing） <196052567+Magic-Abracadabra@users.noreply.github.com>
2026-07-31 20:33:20 +0800

    Delete python数据库模板/冷启动windows directory

--- python数据库模板/冷启动windows/数据库/3/[2, '打开并跳转', [[1, '搜索引擎', '【数据1】'], [1, '其它', '【数据1】']]].py
+++
@@ -1,2 +0,0 @@
-from urllib.parse import quote
-os.system('start "" "'+搜索引擎库[第三层数据[0]]+quote(第三层数据[1])+'"')
--- python数据库模板/冷启动windows/数据库/3/[2, '进化', [[1, '动词', '添加'], [1, '其它', '【数据1】'], [1, '标点符号', '“'], [1, '其它', '【数据2】'], [1, '标点符号', '”']]].py
+++
@@ -1,4 +0,0 @@
-with open(安装目录+f'/数据库/1/{第三层数据[0]}.txt', 'a', encoding='utf-8') as 文件:
-	文件.write(第三层数据[1])
-文件.close()
-print('【渐得如意】添加成功！')
